122738839e8421cd72ab6bb09561e28abdc15954
Sergei <yagelavichus@gmail.com>
2025-12-15 19:36:31 +0200

    Добавил кастомные Exceptions

---
+++ src/main/java/com/yaser/metadata_service/exception/FileNotFoundException.java
@@ -0,0 +1,9 @@
+package com.yaser.metadata_service.exception;
+
+public class FileNotFoundException extends RuntimeException {
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
